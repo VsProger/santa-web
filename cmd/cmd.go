630338a4ab67044607efe8e29ce7e680ce11188d
Baurzhan <220476@astanait.edu.kz>
2024-03-04 20:32:36 +0600

    fix

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -5,19 +5,11 @@ import (
 	"SantaWeb/internal/handlers"
 	"fmt"
 	"github.com/gorilla/mux"
-	"github.com/sirupsen/logrus"
+	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
-	log := logrus.New()
-	log.SetFormatter(&logrus.JSONFormatter{})
-	logFile, _ := os.OpenFile("cmd/logs.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	defer logFile.Close()
-	log.SetOutput(logFile)
-
-	handlers.InitLogger(log)
 
 	err := db.DbConnection()
 	if err != nil {
@@ -29,7 +21,6 @@ func main() {
 
 	port := ":8080"
 
-	log.Infof("Starting server on http://localhost:%s/\n", port)
 	fmt.Printf("Starting server on http://localhost%s/\n", port)
 	log.Fatal(http.ListenAndServe(port, router))
 }
